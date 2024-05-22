aa654f45d7e9be6aa55430d7f07c8427de336e1c
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2024-05-22 21:23:31 +0200

    feat: setup basic app store (theme, loader)

---
+++ store/app.store.ts
@@ -0,0 +1,36 @@
+import { darkTheme, type GlobalTheme } from 'naive-ui';
+import { useStorage as useVStorage } from '@vueuse/core';
+
+export const useAppStore = defineStore('app', () => {
+    const isLoading = ref<boolean>(true)
+    const loadingMessage = ref<string>('')
+
+
+    const isDark = useVStorage('app-theme', false, localStorage);
+    const theme = computed<GlobalTheme | null>(() => (isDark.value ? darkTheme : null));
+  
+    function toggleTheme() {
+        isDark.value = !isDark.value
+        document?.querySelector('body')?.classList.toggle('dark', isDark.value)
+    }
+
+    function startLoading(message?: string) {
+        isLoading.value = true
+        if (message) loadingMessage.value = message
+    }
+
+    function stopLoading() {
+        isLoading.value = false
+        loadingMessage.value = ''
+    }
+
+    return {
+        isDark,
+        theme,
+        isLoading,
+        loadingMessage,
+        startLoading,
+        toggleTheme,
+        stopLoading
+    }
+})
